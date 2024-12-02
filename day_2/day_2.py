ee43588c05c53f36bf1122ff1d04fe5cab2ea5c6
Sibghat.Shah <sibghat.shah01@gmail.com>
2024-12-02 12:19:09 -0500

    added in soln using LIS/LDS for faster runtime

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -7,6 +7,28 @@ set safe count += 1 hten
 
 lets create 2 subfunctions
 len_montonic_
+
+
+We can further optimize by modifying longest increasing subsequnce algorithm by adding in 
+requirements for valid differences between indices and also check for longest decreasing subsequences
+
+with monotonic stack we get: 
+Part 1:
+time complexity: O(N*M)     (we do 2 montonic stack functions that are linear time for each M length row N times (number of rows))
+space complexity: O(N*M)     we have 2 monotonic stack functions that have stack at max size M (number of rows)
+
+Part 2:
+time complexity: O(N*(M^2))     (we do 2 montonic stack functions that are linear time for each M length row N times (number of rows), and iterate over M rows M times to create splices)
+space complexity: O(N*(M^2))     we have 2 monotonic stack functions that have stack at max size M (number of rows), we also create M splices of of new rows 
+
+with LIS algo we can cut down time and space complexity for part 2
+Part 1:
+time complexity: O(N*M)     (we do 2 modified LIS functions that are linear time for each M length row N times (number of rows))
+space complexity: O(N*M)     we have 2 LIS functions that have M size array for LIS/LDS (number of rows)
+
+Part 2:
+time complexity: O(N*(M))      (we do 2 modified LIS functions that are linear time for each M length row N times (number of rows))
+space complexity: O(N*(M))      we have 2 LIS functions that have M size array for LIS/LDS (number of rows) 
 '''
 
 def is_monotonic_decreasing(row):
@@ -38,6 +60,27 @@ def is_monotonic_increasing(row):
     return len(stack) == len(row)
 
 
+def longest_increasing_subsequence(row):
+        LIS = [1] * len(row)
+        valid_diffs = [1, 2, 3]
+
+        for i in range(1, len(row)):
+            for j in range(i):
+                if row[i] > row[j] and abs(row[j] - row[i]) in valid_diffs:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
+
+def longest_decreasing_subsequence(row):
+        LIS = [1] * len(row)
+        valid_diffs = [1, 2, 3]
+
+        for i in range(1, len(row)):
+            for j in range(i):
+                if row[i] < row[j] and abs(row[j] - row[i]) in valid_diffs:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
+
+
 def question_one():
     with open("day_2.txt") as f:
         lines = f.read().split("\n")
@@ -67,11 +110,8 @@ def question_two():
         vals = line.split()
         row = [int(val) for val in vals]
         row_len = len(row)
-
         '''
         check if its initally a monotonically increasing or decreasing valid stack
-
-        lets redo part 1 with this code later
         '''
         if is_monotonic_decreasing(row) or is_monotonic_increasing(row):
             safe_count += 1
@@ -94,6 +134,45 @@ def question_two():
 
     print(safe_count)
 
-question_one()
-question_two()
 
+def question_one_optimized():
+    with open("day_2.txt") as f:
+        lines = f.read().split("\n")
+
+    safe_count = 0
+    valid_diffs = [1, 2, 3]
+
+    for line in lines:
+        vals = line.split()
+        row = [int(val) for val in vals]
+        '''
+        check if its initally a monotonically increasing or decreasing valid stack
+        '''
+        if longest_increasing_subsequence(row) == len(row) or longest_decreasing_subsequence(row) == len(row):
+            safe_count += 1
+
+    print(safe_count)
+
+def question_two_optimized():
+    with open("day_2.txt") as f:
+        lines = f.read().split("\n")
+
+    safe_count = 0
+    valid_diffs = [1, 2, 3]
+
+    for line in lines:
+        vals = line.split()
+        row = [int(val) for val in vals]
+        '''
+        check if its initally a monotonically increasing or decreasing valid stack
+        '''
+        if longest_increasing_subsequence(row) >= len(row) - 1 or longest_decreasing_subsequence(row) >= len(row) - 1:
+            safe_count += 1
+
+    print(safe_count)
+
+
+question_one()
+question_one_optimized()
+question_two()
+question_two_optimized()
