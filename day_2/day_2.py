24097235937f77d13aebdd8d8a025f89ed08845f
Sibghat.Shah <sibghat.shah01@gmail.com>
2024-12-02 11:29:27 -0500

    monotonic stacks for part 1

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,52 +1,4 @@
 '''
-lets check if a row is monotonically increasing or decreasing 
-make sure abs diff 1,2,3
-'''
-
-def question_one():
-    with open("day_2.txt") as f:
-        lines = f.read().split("\n")
-
-    safe_count = 0
-    valid_diffs = [1, 2, 3]
-
-    for line in lines:
-        vals = line.split()
-        vals = [int(val) for val in vals]
-        '''
-        base case check first 2 vals
-        '''
-        curr_diff = vals[1] - vals[0]
-        if curr_diff == 0 or abs(curr_diff) not in valid_diffs:
-            continue
-
-        start_increasing = True if (vals[1] - vals[0] > 0) else False
-        is_valid_row = True
-        '''
-        if we are increasing and our abs diff between curr val and last val
-        is in diff we keep iterating else we move to next line
-        '''
-        row_len = len(vals)
-        i = 2
-
-        while i < len(vals):
-            diff = vals[i] - vals[i - 1]
-            curr_indx_increasing = True if (diff > 0) else False
-
-            if (abs(diff) not in valid_diffs) or curr_indx_increasing != start_increasing or diff == 0:
-                is_valid_row = False
-                break
-            i += 1
-
-        if not is_valid_row:
-            continue
-        else:
-            safe_count += 1
-
-    print(safe_count)
-
-
-'''
 we can do with monotonic stacks
 
 we need to find the lengths of the the monotonically increasing stack and decreasing stack of each row
@@ -85,6 +37,26 @@ def is_monotonic_increasing(row):
 
     return len(stack) == len(row)
 
+
+def question_one():
+    with open("day_2.txt") as f:
+        lines = f.read().split("\n")
+
+    safe_count = 0
+    valid_diffs = [1, 2, 3]
+
+    for line in lines:
+        vals = line.split()
+        row = [int(val) for val in vals]
+        '''
+        check if its initally a monotonically increasing or decreasing valid stack
+        '''
+        if is_monotonic_decreasing(row) or is_monotonic_increasing(row):
+            safe_count += 1
+
+    print(safe_count)
+
+
 def question_two():
     with open("day_2.txt") as f:
         lines = f.read().split("\n")
@@ -122,6 +94,6 @@ def question_two():
 
     print(safe_count)
 
-# question_one()
+question_one()
 question_two()
 
